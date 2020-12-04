f9a1318e7adb809e530d65683eb14ab0692c3fbe
Benjamin <1234hteimaj4321@gmail.com>
2020-12-04 15:07:00 -0700

    got rid of canvas

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const canvas = document.querySelector("canvas");
-// const ctx = canvas.getContext("2d");
-// ctx.fillStyle = "green";
-// ctx.fillStyle = "red";
-// ctx.fillRect(20, 20, 100, 100);
-// ctx.fillRect(10, 10, 30, 30);
+const injector = document.querySelector("#injector");
 
 const small = [20, 20];
 const med = [30, 30];
 const large = [40, 40];
 
+const slow = 5;
+const meh = 10;
+const fast = 15;
+
 class Character {
-  constructor(name, size, color, health, speed, isEnemy) {
+  constructor(name, size, color, health, speed, isEnemy, loc) {
     this.name = name;
     this.size = size;
     this.color = color;
     this.health = health;
     this.speed = speed;
     this.isEnemy = isEnemy;
+    this.loc = loc;
   }
   walk = {
-    up: () => {},
+    up: () => {
+      this.loc[1] -= this.speed;
+      this.updateLoc();
+    },
     right: () => {},
     down: () => {},
     left: () => {},
@@ -35,16 +38,8 @@ class Character {
     heavy: () => {},
     ranged: () => {},
   };
-  spawn() {
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = this.color;
-    ctx.fillRect(20, 20, this.size[0], this.size[1]);
-  }
+  spawn() {}
+  updateLoc() {}
 }
 
-const main = new Character("Ben", med, "red", 100, "fast", false);
-
-// main.attack.light();
-// main.walk.up();
-
-main.spawn();
+const main = new Character("Ben", med, "red", 100, fast, false, [20, 20]);
