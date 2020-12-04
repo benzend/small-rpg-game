1cd60b6869815e0b42df7f815daf69377f222cb7
Benjamin <1234hteimaj4321@gmail.com>
2020-12-04 16:04:39 -0700

    added lose health functionality and added healthbar, and name to display on each character

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const injector = document.querySelector("#injector");
 
+// HTML
+
 const small = [20, 20];
 const med = [30, 30];
 const large = [40, 40];
@@ -9,7 +11,7 @@ const meh = 10;
 const fast = 15;
 
 class Character {
-  constructor(name, size, color, health, speed, isEnemy, loc) {
+  constructor(name, size, color, health, speed, isEnemy, loc = [20, 20]) {
     this.name = name;
     this.size = size;
     this.color = color;
@@ -23,9 +25,18 @@ class Character {
       this.loc[1] -= this.speed;
       this.updateLoc();
     },
-    right: () => {},
-    down: () => {},
-    left: () => {},
+    right: () => {
+      this.loc[0] += this.speed;
+      this.updateLoc();
+    },
+    down: () => {
+      this.loc[1] += this.speed;
+      this.updateLoc();
+    },
+    left: () => {
+      this.loc[0] -= this.speed;
+      this.updateLoc();
+    },
   };
   run = {
     up: () => {},
@@ -38,8 +49,66 @@ class Character {
     heavy: () => {},
     ranged: () => {},
   };
-  spawn() {}
-  updateLoc() {}
+  gainHealth() {
+    this.health += 20;
+    this.updateHealth();
+  }
+  loseHealth() {
+    this.health -= 20;
+    this.updateHealth();
+  }
+  spawn() {
+    const character = document.createElement("div");
+    const name = document.createElement("div");
+    const healthBar = document.createElement("div");
+    const healthBarInner = document.createElement("div");
+    character.id = this.name;
+    character.className = "character";
+    character.style.width = this.size[0] + "px";
+    character.style.height = this.size[1] + "px";
+    character.style.left = this.loc[0] + "px";
+    character.style.top = this.loc[1] + "px";
+    character.style.backgroundColor = this.color;
+    name.innerHTML = this.name;
+    name.className = "name";
+    name.style.height = "5px";
+    name.style.position = "relative";
+    name.style.top = "-20px";
+    healthBar.style.height = "5px";
+    healthBar.style.width = this.health + "px";
+    healthBar.style.position = "relative";
+    healthBar.style.top = "-30px";
+    healthBar.style.backgroundColor = "white";
+    healthBar.style.border = "solid 1px black";
+    healthBar.className = "health-bar";
+    healthBarInner.style.width = "100%";
+    healthBarInner.style.height = "100%";
+    healthBarInner.style.backgroundColor = "red";
+    healthBarInner.className = "health-bar__inner";
+    healthBar.append(healthBarInner);
+    character.append(healthBar);
+    character.append(name);
+    injector.append(character);
+  }
+  updateLoc() {
+    const character = document.querySelector(`#${this.name}`);
+    character.style.left = this.loc[0] + "px";
+    character.style.top = this.loc[1] + "px";
+  }
+  updateHealth() {
+    const characterHealth = document.querySelector(
+      `#${this.name} .health-bar__inner`
+    );
+    characterHealth.style.width = this.health + "px";
+  }
 }
 
-const main = new Character("Ben", med, "red", 100, fast, false, [20, 20]);
+const main = new Character("Ben", med, "red", 100, fast, false);
+const secondary = new Character("Zach", large, "blue", 100, slow, false, [
+  100,
+  100,
+]);
+
+main.spawn();
+main.walk.down();
+secondary.spawn();
