c1c4d1cbe1a2a3c9d0c7c4cc53340a8ea85895d1
Benjamin <1234hteimaj4321@gmail.com>
2020-12-04 14:29:07 -0700

    added spawn func

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 30, 30);
+// const ctx = canvas.getContext("2d");
+// ctx.fillStyle = "green";
+// ctx.fillStyle = "red";
+// ctx.fillRect(20, 20, 100, 100);
+// ctx.fillRect(10, 10, 30, 30);
+
+const small = [20, 20];
+const med = [30, 30];
+const large = [40, 40];
 
 class Character {
-  constructor(name, size, color, health, speed) {
+  constructor(name, size, color, health, speed, isEnemy) {
     this.name = name;
     this.size = size;
     this.color = color;
     this.health = health;
     this.speed = speed;
+    this.isEnemy = isEnemy;
   }
   walk = {
     up: () => {},
@@ -28,9 +35,16 @@ class Character {
     heavy: () => {},
     ranged: () => {},
   };
+  spawn() {
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = this.color;
+    ctx.fillRect(20, 20, this.size[0], this.size[1]);
+  }
 }
 
-const main = new Character("Ben", "large", "red", 100, "fast");
+const main = new Character("Ben", med, "red", 100, "fast", false);
 
-main.attack.light();
-main.walk.up();
+// main.attack.light();
+// main.walk.up();
+
+main.spawn();
